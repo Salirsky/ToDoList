9d1dba87d2c4e51f27df4ee8e14dbac6cc466142
Marina Belova <salirsky@mail.ru>
2022-04-20 14:20:57 +0300

    Added interaction with localStorage

--- js/script.js
+++ js/script.js
@@ -5,12 +5,22 @@ const todoCompleted = document.querySelector(".todo-completed");
 //const todoRemove = document.querySelector(".todo-remove");
 
 // Массив для новых задач:
-const toDoData = [];
+let toDoData = [];
 
 // Функция-рендер, которая будет отрисовывать все наши toDo:
 const render = function () {
+  // Сохранять данные массива toDoData в localStorage:
+  localStorage.setItem("toDoData", JSON.stringify(toDoData));
+  console.log("toDoData", JSON.stringify(toDoData));
+  // Получаем массив обратно:
+  toDoData = JSON.parse(localStorage.getItem("toDoData"));
+  console.log(toDoData);
+  // Здесь хотя бы галочки "выполнено" отрабатывают, но тоже при обновлении страницы инфа на странице не сохраняется.
+  // Сейчас я не могу удалить из хранилища один элемент списка
+
   todoList.innerHTML = ""; // Обнуляем список невыполненных задач
   todoCompleted.innerHTML = ""; // Обнуляем список выполненных задач
+
   toDoData.forEach(function (item) {
     const li = document.createElement("li"); // создаём новый элемент li.
     li.classList.add("todo-item");
@@ -41,6 +51,7 @@ const render = function () {
     // Удаляем элемент по клику на корзину:
     li.querySelector(".todo-remove").addEventListener("click", function () {
       li.remove();
+      localStorage.removeItem("toDoData"); // Нужно передавать "key", по которому будет удаляться значение из хранилища. Сейчас наш "key" -весь массив со всеми данными. Для отдельных элементов списка у нас нет ключа.
     });
   });
 };
@@ -54,18 +65,39 @@ todoControl.addEventListener("submit", function (event) {
     text: headerInput.value,
     completed: false,
   };
-  console.log(headerInput.value);
+  //console.log(headerInput.value);
   // Проверяем, содержит ли headerInput.value данные
   if (headerInput.value !== "") {
     // Отправляем новый объект в массив toDoData:
     toDoData.push(newToDo);
+
+    // В localStorage данные есть, но при перезагрузке страницы они не появляются на странице и не реагируют на кнопку "выполнено"
     headerInput.value = ""; // очищаем поле ввода
+
     render(); // render выводит содержимое массива toDoData
   }
 });
 
+// const storageArr = function () {
+//   // Сохранять данные массива toDoData в localStorage:
+//   localStorage.setItem("toDoData", JSON.stringify(toDoData));
+//   console.log("toDoData", JSON.stringify(toDoData));
+//   // Получаем массив обратно:
+//   toDoData = JSON.parse(localStorage.getItem("toDoData"));
+//   console.log(toDoData);
+// };
+
+// // Через строку работать не будет
+// const storageStr = function () {
+//   // Сохранять данные массива toDoData в localStorage:
+//   localStorage.setItem("newToDo", JSON.stringify(newToDo));
+//   console.log("newToDo", JSON.stringify(newToDo));
+//   // Получаем массив обратно:
+//   newToDo = JSON.parse(localStorage.getItem("newToDo"));
+//   console.log(newToDo);
+// };
 // ДЗ:
-// Реализовать удаление задач по нажатию на корзину
+// \/ Реализовать удаление задач по нажатию на корзину
 // \/ Запретить добавлять задачи, пока value пуст
 // При каждом добавлении задачи массив todoData сохранять в localStorage
 // А при перезагрузке страницы необходимо извлекать эту информацию и заносить её в todoData
